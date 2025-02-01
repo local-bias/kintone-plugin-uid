0331e1d535be9e3f4463b72185a472c1d1e1c30e
Ribbit <echo07200503@gmail.com>
2025-02-01 09:28:31 +0900

    🔥 不要なコードを削除し、コンポーネントを簡素化

--- src/config/components/model/form/common/index.tsx
+++ src/config/components/model/form/common/index.tsx
@@ -1,20 +1,5 @@
-import { getCommonPropertyAtom } from '@/config/states/plugin';
-import { t } from '@/lib/i18n';
-import { JotaiText } from '@konomi-app/kintone-utilities-jotai';
-import {
-  PluginFormDescription,
-  PluginFormSection,
-  PluginFormTitle,
-} from '@konomi-app/kintone-utilities-react';
 import { FC } from 'react';
 
-const Component: FC = () => (
-  <div className=''>
-    <iframe
-      src='http://localhost:4321/kintone-plugin/common-config'
-      className='w-full h-[720px] border-0'
-    ></iframe>
-  </div>
-);
+const Component: FC = () => <div className=''></div>;
 
 export default Component;
