3e8e8e2b027a918ea4a99d1d2a4bb35ca1152e1e
Ribbit <echo07200503@gmail.com>
2025-02-01 09:26:48 +0900

    📦 プラグイン設定の状態管理を改善し、条件および共通設定のアトムをリファクタリング

--- src/config/states/plugin.ts
+++ src/config/states/plugin.ts
@@ -8,40 +8,79 @@ import { produce } from 'immer';
 import { atom, PrimitiveAtom } from 'jotai';
 import { focusAtom } from 'jotai-optics';
 import { atomWithDefault } from 'jotai/utils';
+import { SetStateAction } from 'react';
 
 export const pluginConfigAtom = atom<PluginConfig>(restorePluginConfig());
 export const loadingAtom = atom(false);
-export const conditionsAtom = focusAtom(pluginConfigAtom, (s) => s.prop('conditions'));
-export const conditionsLengthAtom = focusAtom(conditionsAtom, (s) => s.prop('length'));
-export const commonConfigAtom = focusAtom(pluginConfigAtom, (s) => s.prop('common'));
+// export const pluginConditionsAtom = focusAtom(pluginConfigAtom, (s) => s.prop('conditions'));
+export const pluginConditionsAtom = atom(
+  (get) => get(pluginConfigAtom).conditions,
+  (_, set, newValue: SetStateAction<PluginCondition[]>) => {
+    set(pluginConfigAtom, (current) =>
+      produce(current, (draft) => {
+        draft.conditions = typeof newValue === 'function' ? newValue(draft.conditions) : newValue;
+      })
+    );
+  }
+);
+export const conditionsLengthAtom = atom((get) => get(pluginConditionsAtom).length);
+// export const pluginCommonConfigAtom = focusAtom(pluginConfigAtom, (s) => s.prop('common'));
+export const pluginCommonConfigAtom = atom(
+  (get) => get(pluginConfigAtom).common,
+  (_, set, newValue: SetStateAction<PluginCommonConfig>) => {
+    set(pluginConfigAtom, (current) =>
+      produce(current, (draft) => {
+        draft.common = typeof newValue === 'function' ? newValue(draft.common) : newValue;
+      })
+    );
+  }
+);
 export const selectedConditionIdAtom = atomWithDefault<string | null>(
-  (get) => get(conditionsAtom)[0]?.id ?? null
+  (get) => get(pluginConditionsAtom)[0]?.id ?? null
 );
 export const commonSettingsShownAtom = atom((get) => get(selectedConditionIdAtom) === null);
 export const selectedConditionAtom = atom(
   (get) => {
-    const conditions = get(conditionsAtom);
+    const conditions = get(pluginConditionsAtom);
     const selectedConditionId = get(selectedConditionIdAtom);
     return conditions.find((condition) => condition.id === selectedConditionId) ?? conditions[0]!;
   },
-  (get, set, newValue: PluginCondition) => {
+  (get, set, newValue: SetStateAction<PluginCondition>) => {
     const selectedConditionId = get(selectedConditionIdAtom);
-    set(conditionsAtom, (current) =>
+    const conditions = get(pluginConditionsAtom);
+    const index = conditions.findIndex((condition) => condition.id === selectedConditionId);
+    if (index === -1) {
+      return;
+    }
+    set(pluginConfigAtom, (current) =>
       produce(current, (draft) => {
-        const index = draft.findIndex((condition) => condition.id === selectedConditionId);
-        if (index !== -1) {
-          draft[index] = newValue;
-        }
+        draft.conditions[index] =
+          typeof newValue === 'function' ? newValue(draft.conditions[index]!) : newValue;
       })
     );
   }
 );
 
 export const getCommonPropertyAtom = <T extends keyof PluginCommonConfig>(property: T) =>
-  focusAtom(commonConfigAtom, (s) => s.prop(property)) as PrimitiveAtom<PluginCommonConfig[T]>;
+  focusAtom(pluginCommonConfigAtom, (s) => s.prop(property)) as PrimitiveAtom<
+    PluginCommonConfig[T]
+  >;
 
+// export const getConditionPropertyAtom = <T extends keyof PluginCondition>(property: T) =>
+//   focusAtom(selectedConditionAtom, (s) => s.prop(property)) as PrimitiveAtom<PluginCondition[T]>;
 export const getConditionPropertyAtom = <T extends keyof PluginCondition>(property: T) =>
-  focusAtom(selectedConditionAtom, (s) => s.prop(property)) as PrimitiveAtom<PluginCondition[T]>;
+  atom(
+    (get) => {
+      return get(selectedConditionAtom)[property];
+    },
+    (_, set, newValue: SetStateAction<PluginCondition[T]>) => {
+      set(selectedConditionAtom, (condition) =>
+        produce(condition, (draft) => {
+          draft[property] = typeof newValue === 'function' ? newValue(draft[property]) : newValue;
+        })
+      );
+    }
+  );
 
 export const modeAtom = getConditionPropertyAtom('mode');
 export const isIDRegenerateButtonShownAtom = getConditionPropertyAtom('isIDRegenerateButtonShown');
